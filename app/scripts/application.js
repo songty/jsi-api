195beb33f710774568c89733585f627a7cadc8f9
Tian Song <tian.y.song@gmail.com>
2014-06-30 15:03:09 -0700

    shows individual employee's projects.

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -12,5 +12,12 @@ App.ApplicationAdapter = DS.RESTAdapter.extend({
 App.Employee = DS.Model.extend({
   name: DS.attr('string'),
   role: DS.attr('string'),
-  boss: DS.belongsTo('employee', {async: true})
+  boss: DS.belongsTo('employee', {async: true}),
+  projects: DS.hasMany('projects', {async: true}),
+  reports: DS.hasMany('employee', {async: true})
+});
+
+App.Project = DS.Model.extend({
+	teamMembers: DS.hasMany('employee', {async: true}),
+	name: DS.attr('string')
 });
