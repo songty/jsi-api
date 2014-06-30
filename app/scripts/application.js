b63710064c8396b912c9645af19bfe5999024391
Ariel Spear <simpleredfox@gmail.com>
2014-06-30 13:15:21 -0700

    now displaying data, but some still objects within the object

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -5,11 +5,12 @@ var App = window.App = Ember.Application.create();
 require('./router.js')(App);
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-  host: 'http://localhost:9000/api/v1/employees'
+  namespace: 'api/v1',
+  host: 'http://localhost:9000'
 });
 
 App.Employee = DS.Model.extend({
-  id: DS.attr('string'),
+  eployeeID: DS.attr('string'),
   name: DS.attr('string'),
   role: DS.attr('string'),
   links: DS.attr('string')
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,4 +3,9 @@ module.exports = function(App){
   App.Router.map(function(){
     console.log('I\'m the router');
   });
+  App.IndexRoute = Ember.Route.extend({
+    model: function(){
+      return this.store.find('employee');
+    }
+  });
 };
